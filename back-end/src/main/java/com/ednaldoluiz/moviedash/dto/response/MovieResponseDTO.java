6231833aeefd8e5590f0d7efe3c219eb6753c8de
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-04-18 08:00:00 -0300

    fix: implement serializable to match new redis jdk serialization

--- back-end/src/main/java/com/ednaldoluiz/moviedash/dto/response/MovieResponseDTO.java
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/dto/response/MovieResponseDTO.java
@@ -1,19 +1,25 @@
 package com.ednaldoluiz.moviedash.dto.response;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.ednaldoluiz.moviedash.model.Movie;
+import com.ednaldoluiz.moviedash.repository.projection.MovieProjection;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public record MovieResponseDTO(
+public record MovieResponseDTO (
 
     String title,
     Double voteAverage,
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    LocalDate releaseDate) {
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    LocalDate releaseDate) implements Serializable {
 
         public MovieResponseDTO(Movie movie) {
+            this(movie.getTitle(), movie.getVoteAverage(), movie.getReleaseDate());
+        }
+
+        public MovieResponseDTO(MovieProjection movie) {
             this(movie.getTitle(), movie.getVoteAverage(), movie.getReleaseDate());
         }
     }

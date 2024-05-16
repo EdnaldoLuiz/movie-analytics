ce17ce44d1a8b2b542c30a5d86ef0099d0448ee3
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-05-16 08:00:00 -0300

    chore: changed generic params nomenclature and added a method comment

--- back-end/src/main/java/com/ednaldoluiz/moviedash/service/impl/MovieServiceImpl.java
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/service/impl/MovieServiceImpl.java
@@ -3,6 +3,7 @@ package com.ednaldoluiz.moviedash.service.impl;
 import java.util.List;
 import java.util.function.Function;
 
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@CacheConfig(cacheNames = MOVIE_CACHE)
 @RequiredArgsConstructor
 public class MovieServiceImpl implements MovieService {
 
@@ -27,7 +29,7 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     @Cacheable(
-        cacheNames = MOVIE_CACHE, 
+        cacheNames = "all", 
         key = "{#pageable.pageNumber,#pageable.pageSize,#pageable.sort}", 
         unless = "#result.numberOfElements < 5")
     public Page<MovieResponseDTO> findAllMovies(Pageable pageable) {
@@ -37,7 +39,7 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     @Cacheable(
-        cacheNames = MOVIE_CACHE, 
+        cacheNames = "top10", 
         key = "{#pageable.pageNumber,#pageable.pageSize,#pageable.sort,#genreIds}", 
         unless = "#result.numberOfElements < 5")
     public Page<MovieResponseDTO> findTop10Movies(Pageable pageable, List<Long> genreIds) {
@@ -47,7 +49,7 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     @Cacheable(
-        cacheNames = MOVIE_CACHE, 
+        cacheNames = "top5", 
         key = "{#genreIds, #year}", 
         unless = "#result.numberOfElements == 5")
     public Page<MovieResponseDTO> findTop5MoviesByYear(Pageable pageable, List<Long> genreIds, Integer year) {
@@ -61,8 +63,17 @@ public class MovieServiceImpl implements MovieService {
         return repository.findByTitleContainingIgnoreCase(keyword, pageable);
     }
 
-    private <T, M> Page<M> convertPage(Page<T> page, Function<T, M> converter) {
-        List<M> content = page.getContent()
+    /**
+     * Este método converte uma página de projeções em uma página de DTOs de forma generica.
+     * 
+     * @param page      a página de projeções
+     * @param converter a função de conversão de projeção para DTO  
+     *            
+     * @return a página de DTOs
+     */
+
+    private <PROJECTION, DTO> Page<DTO> convertPage(Page<PROJECTION> page, Function<PROJECTION, DTO> converter) {
+        List<DTO> content = page.getContent()
             .stream()
             .map(converter)
             .toList();

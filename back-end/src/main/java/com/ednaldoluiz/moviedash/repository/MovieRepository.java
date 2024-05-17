fdd5c02e6230646036f96ba9dced98037ab24dbc
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-05-17 08:00:00 -0300

    chore: removed specifications from MovieRepository

--- back-end/src/main/java/com/ednaldoluiz/moviedash/repository/MovieRepository.java
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/repository/MovieRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +13,7 @@ import com.ednaldoluiz.moviedash.model.Movie;
 import com.ednaldoluiz.moviedash.repository.projection.MovieProjection;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     Optional<Movie> findByTitle(String title);
 
@@ -23,7 +22,6 @@ public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecific
                 FROM Movie m
                 INNER JOIN m.genres g
                 WHERE g.id IN :genreIds
-                ORDER BY m.voteAverage DESC
             """)
     Page<MovieProjection> findTop10ByGenreAndVoteAverage(Pageable pageable, List<Long> genreIds);
 
@@ -31,7 +29,6 @@ public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecific
                 SELECT m
                 FROM Movie m
                 WHERE m.title LIKE %:keyword%
-                ORDER BY m.voteAverage DESC
             """)
     Page<Movie> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
 
@@ -40,8 +37,7 @@ public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecific
                 FROM Movie m
                 INNER JOIN m.genres g
                 WHERE g.id IN :genreIds AND YEAR(m.releaseDate) = :year
-                ORDER BY m.voteAverage DESC
             """)
     Page<MovieProjection> findTop5ByGenreAndVoteAverageInYear(Pageable pageable, List<Long> genreIds, Integer year);
-    
+
 }

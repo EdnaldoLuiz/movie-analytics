b94b0ef4faf008e36690db3cfdc0b7684bc6185a
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-05-25 10:00:00 -0300

    feat: added file fields headers in utils

--- back-end/src/main/java/com/ednaldoluiz/moviedash/utils/APIUtils.java
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/utils/APIUtils.java
@@ -10,12 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class APIUtils {
 
+    public static final String CONTENT_DISPOSITION = "Content-Disposition";
+
+    public static final String[] FILE_HEADER = { "ID", "Título", "Conteúdo adulto", "Idioma", "Data de Lançamento",
+            "Popularidade",
+            "Nota Média", "Quantidade de Votos", "Gêneros" };
+
+    public static final String ATTACHMENT = "attachment; filename=";
     public static final String PAGE_NUMBER = "1";
     public static final String PAGE_SIZE = "10";
     public static final String SORT_DEFAULT = "voteAverage";
 
     public static Sort getSort(MovieSortType sort, String direction) {
-        return direction.equalsIgnoreCase("ASC") ? 
-        Sort.by(sort.getValue()).ascending() : Sort.by(sort.getValue()).descending();
+        return direction.equalsIgnoreCase("ASC") ? Sort.by(sort.getValue()).ascending()
+                : Sort.by(sort.getValue()).descending();
     }
 }

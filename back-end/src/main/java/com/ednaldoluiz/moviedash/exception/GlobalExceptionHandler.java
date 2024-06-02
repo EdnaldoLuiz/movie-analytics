bb29bc62d8ca3994be684fc21754acea01940d09
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-06-02 19:28:05 -0300

    feat: add IOException to global exception handler

--- back-end/src/main/java/com/ednaldoluiz/moviedash/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/exception/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,5 +34,11 @@ public final class GlobalExceptionHandler {
             errors.add(new ErrorResponse(error.getField(), error.getDefaultMessage()));
         });
         return errors;
+    }
+
+    @ExceptionHandler({IOException.class})
+    public ResponseEntity<String> handleIOExceptions(IOException e) {
+        log.error("Ocorreu um erro em 'handleIOException': {}", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }

cbab374e890157afd641900e5f74ed97f5b14161
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-06-09 20:12:11 -0300

    feat: add new function to sort Maps by value

--- back-end/src/main/java/com/ednaldoluiz/moviedash/utils/APIUtils.java
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/utils/APIUtils.java
@@ -1,5 +1,11 @@
 package com.ednaldoluiz.moviedash.utils;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.springframework.data.domain.Sort;
 
 import com.ednaldoluiz.moviedash.model.enums.MovieSortType;
@@ -24,5 +30,17 @@ public final class APIUtils {
     public static Sort getSort(MovieSortType sort, String direction) {
         return direction.equalsIgnoreCase("ASC") ? Sort.by(sort.getValue()).ascending()
                 : Sort.by(sort.getValue()).descending();
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Entry.comparingByValue());
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
     }
 }

f363fd5f6a8d4a3601fba51f2ef5db5df85f14d3
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-06-10 08:00:00 -0300

    feat: create new BeansConstants for beans management

---
+++ back-end/src/main/java/com/ednaldoluiz/moviedash/constant/BeanConstants.java
@@ -0,0 +1,13 @@
+package com.ednaldoluiz.moviedash.constant;
+
+public interface BeanConstants {
+
+    String REDIS_TEMPLATE = "redisTemplate";
+    String REDIS_CACHE_MANAGER = "cacheManager";
+    String REDIS_CACHE_CONFIGURATION = "cacheConfiguration";
+    String REDIS_CACHE_MANAGER_BUILDER_CUSTOMIZER = "redisCacheManagerBuilderCustomizer";
+    String REST_TEMPLATE = "restTemplate";
+    String OPEN_API = "customOpenAPI";
+    String SCHEDULE_TASK = "scheduleTask";
+
+}
